6ac3421f7ebbc2e1cc5627c7204c52faf088b203
dirk <melowdee@gmail.com>
2019-05-05 12:36:28 +0200

    css  änderung

--- header.php
+++ header.php
@@ -14,10 +14,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <!-- TrustBox script -->
     <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- End Trustbox script -->
 <?php wp_head(); ?>
 </head>
@@ -29,7 +30,7 @@
 	<header id="masthead" class="site-header" role="banner">
         <div class="contact-data clearfix">
         <div class="floatleft">
-            <i class="glyphicon glyphicon-call white" :<?php _e('+49 40 855 098 23', 'traduset');?> - <?php _e('E-mail', 'traduset');?>: <a href="mailto:<?php _e('info@traduset.de', 'traduset');?>"><?php _e('info@traduset.de', 'traduset');?></a>
+            <i class="glyphicon glyphicon-call white"></i> :<?php _e('+49 40 855 098 23', 'traduset');?> - <?php _e('E-mail', 'traduset');?>: <a href="mailto:<?php _e('info@traduset.de', 'traduset');?>"><?php _e('info@traduset.de', 'traduset');?></a>
         </div>
         <div id="header-right">
             <ul class="languages">
