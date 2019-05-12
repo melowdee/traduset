c63c1b2f27e194a09ef48229eff1250267f1cc95
dirk <melowdee@gmail.com>
2019-05-12 11:43:04 +0200

    css und header icons icons

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 	<header id="masthead" class="site-header" role="banner">
         <div class="contact-data clearfix">
         <div class="floatleft">
-            <i class="material-icons" style="font-size:20px;color:#ffffff;margin-right: 5px;">phone</i> <?php _e('+49 40 855 098 23', 'traduset');?>   -   <i class="material-icons" style="font-size:20px;color:#ffffff;margin-right: 5px;">email</i> <a href="mailto:<?php _e('info@traduset.de', 'traduset');?>"><?php _e('info@traduset.de', 'traduset');?></a>
+            <span class="header-icons"><i class="material-icons">phone</i></span> <?php _e('+49 40 855 098 23', 'traduset');?>&nbsp;&nbsp;&nbsp;<span class="header-icons"><i class="material-icons">email</i><span class="header-icons"><a href="mailto:<?php _e('info@traduset.de', 'traduset');?>"><?php _e('info@traduset.de', 'traduset');?></a>
         </div>
         <div id="header-right">
             <ul class="languages">
